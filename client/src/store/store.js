944061912205b5faa09a4e825c5c3fccedfa05ee
t1ep1l0t <alien95111@gmail.com>
2023-04-04 21:49:06 +0300

    initial commit

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -5,12 +5,25 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
     state: {
-        menu: false
+        menu: false,
+        popular: undefined
     },
     mutations: {
         set_menu (state) {
             state.menu = !state.menu
+        },
+        set_popular (state, payload) {
+            state.popular = payload
         }
+    },
+    actions: {
+        async get_popular () {
+            const response = await fetch('http://localhost:5000/api/populars/get-all')
+            const payload = await response.json();
+
+            this.commit('set_popular', payload);
+        }
+
     }
 })
 
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,33 +1,83 @@
 <template>
-<section class="top">
-  <div class="container">
-    <div class="top__inner">
-      <h1 class="top__title">
-        Студия лазерной эпиляции
-        <span>Moscow Lazerpil</span>
-      </h1>
-      <p class="top__desc">
-        По окончанию курса процедур выдаем Сертификат сроком 12 месяцев. Гарантируем бесплатное обслуживание в случае если результат не достигнут
-      </p>
-      <router-link class="top__button" to="/contact-us">записаться на <br> консультацию</router-link>
-    </div>
-    <div class="top__links">
-      <router-link to="/services" class="top__card top__card__woman">
-        <h2 class="top__card__title">Эпиляция</h2>
-        <span class="top__card__name">Женская</span>
-      </router-link>
-      <router-link to="/services" class="top__card top__card__man">
-        <h2 class="top__card__title">Эпиляция</h2>
-        <span class="top__card__name">Мужская</span>
-      </router-link>
-    </div>
+  <div class="home">
+    <section class="top">
+      <div class="container">
+        <div class="top__inner">
+          <h1 class="top__title">
+            Студия лазерной эпиляции
+            <span>Moscow Lazerpil</span>
+          </h1>
+          <p class="top__desc">
+            По окончанию курса процедур выдаем Сертификат сроком 12 месяцев. Гарантируем бесплатное обслуживание в случае если результат не достигнут
+          </p>
+          <router-link class="top__button" to="/contact-us">записаться на <br> консультацию</router-link>
+        </div>
+        <div class="top__links">
+          <router-link to="/services" class="top__card top__card__woman">
+            <h2 class="top__card__title">Эпиляция</h2>
+            <span class="top__card__name">Женская</span>
+          </router-link>
+          <router-link to="/services" class="top__card top__card__man">
+            <h2 class="top__card__title">Эпиляция</h2>
+            <span class="top__card__name">Мужская</span>
+          </router-link>
+        </div>
+      </div>
+    </section>
+
+    <section class="popular">
+      <div class="container">
+        <div class="popular__inner">
+          <div class="popular__head">
+            <h2 class="popular__title">ПОПУЛЯРНЫЕ УСЛУГИ</h2>
+          </div>
+          <div class="popular__content">
+            <VueSlickCarousel class="popular__slider"
+                              v-if="$store.state.popular"
+                              v-bind="slider_settings"
+            >
+              <div class="popular__slide"
+                   v-for="item of $store.state.popular"
+                   :key="item._id"
+              >
+                    <img :src="'/popular_images/' + item.picture" alt="" class="popular__slide__img">
+<!--                <img src="../../dist/popular_images/5ea4d8f6-83f5-40c1-a1be-a4385c735806.jpg" alt="" class="popular__slide__img">-->
+                <h3 class="popular__slide__title">{{ item.name }}</h3>
+                <span class="popular__slide__desc">{{ item.desc }}</span>
+                <span class="popular__slide__price">{{ item.price }} ₽</span>
+              </div>
+            </VueSlickCarousel>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
-</section>
 </template>
 
 <script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 export default {
-  name: "HomeView"
+  name: "HomeView",
+  components: {
+    VueSlickCarousel
+  },
+  data () {
+    return {
+      slider_settings: {
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        slidesToScroll: 1,
+        slidesToShow: 4,
+        swipeToSlide: true
+      }
+    }
+  },
+  mounted () {
+    this.$store.dispatch('get_popular');
+  }
 }
 </script>
 
@@ -189,6 +239,66 @@ export default {
         text-transform: uppercase;
         align-self: center;
       }
+    }
+  }
+}
+
+.popular {
+  width: 100%;
+  margin-top: 120px;
+
+  &__inner {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    gap: 60px;
+  }
+  &__head  {
+    width: fit-content;
+    align-self: center;
+  }
+  &__title {
+    font-weight: 600;
+    font-size: 28px;
+    line-height: 32px;
+  }
+  &__content {
+    width: 100%;
+  }
+  &__slider {
+    width: 100%;
+  }
+  &__slide {
+    max-width: 285px;
+    height: 460px;
+    margin: 0 10px;
+    display: flex!important;
+    flex-direction: column;
+    align-items: start;
+    gap: 16px;
+
+    &__img {
+      width: 100%;
+      height: auto;
+      margin-bottom: 14px;
+    }
+    &__name {
+      font-weight: 700;
+      font-size: 18px;
+      line-height: 130%;
+      text-transform: uppercase;
+      color: $rose_light;
+    }
+    &__desc {
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 130%;
+    }
+    &__price {
+      font-weight: 700;
+      font-size: 20px;
+      line-height: 130%;
     }
   }
 }
