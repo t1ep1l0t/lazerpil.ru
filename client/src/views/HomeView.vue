e759acecf68c1079424a39f6e58851f97ae846a2
t1ep1l0t <alien95111@gmail.com>
2023-04-05 03:14:28 +0300

    initial commit

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -121,6 +121,57 @@
         </div>
       </div>
     </section>
+    <section class="gallery">
+      <div class="container">
+        <div class="gallery__inner">
+          <div class="gallery__head">
+            <h2 class="gallery__title">Галерея</h2>
+          </div>
+          <div class="gallery__content">
+            <div class="gallery__image"></div>
+            <div class="gallery__image"></div>
+            <div class="gallery__image"></div>
+            <div class="gallery__image"></div>
+            <div class="gallery__image"></div>
+            <div class="gallery__image"></div>
+          </div>
+          <router-link class="gallery__link" to="/gallery">ПОКАЗАТЬ ЕЩЕ</router-link>
+        </div>
+      </div>
+    </section>
+    <section class="location">
+      <div class="container">
+        <div class="location__inner">
+          <div class="location__head">
+            <h2 class="location__title">Наши локации</h2>
+          </div>
+          <div class="location__content">
+            <div class="location__item"
+                 v-for="(loaction, index) of locations"
+                 :key="index"
+            >
+              <VueSlickCarousel class="location__slider">
+                <div class="location__slide"
+                     v-for="(img, index) of loaction.photos"
+                     :key="index"
+                >
+                  <img class="img" :src="img" alt="">
+                </div>
+              </VueSlickCarousel>
+              <span class="location__metro"
+                    :style="{color: loaction.color}"
+              >
+                <svg width="35" height="24" viewBox="0 0 35 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M32.592 20.6473L24.416 0L17.5 12.0698L10.612 0L2.408 20.6473H0V23.7765H12.376V20.6473H10.528L12.32 15.5064L17.5 24L22.68 15.5064L24.472 20.6473H22.624V23.7765H35V20.6473H32.592Z" :fill="loaction.color"/>
+                </svg>
+                {{ loaction.metro }}
+              </span>
+              <span class="location__address">{{ loaction.address }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -436,6 +487,26 @@ export default {
             },
           ]
         },
+      ],
+      locations: [
+        {
+          metro: 'Бульвар Рокассовского',
+          address: 'улица Бойцовая, дом 27',
+          color: '#EC232B',
+          photos: [
+              'https://i.ibb.co/DgmCpy6/location1.png',
+              'https://i.ibb.co/qCWJGP2/location2.png'
+          ]
+        },
+        {
+          metro: 'Алексеевская',
+          address: 'улица Проспект Мира, 95с1',
+          color: '#F58220',
+          photos: [
+            'https://i.ibb.co/qCWJGP2/location2.png',
+            'https://i.ibb.co/DgmCpy6/location1.png'
+          ]
+        },
       ]
     }
   },
@@ -626,6 +697,7 @@ export default {
     font-weight: 600;
     font-size: 28px;
     line-height: 32px;
+    text-transform: uppercase;
 
     @media(max-width: 768px) {
       font-size: 22px;
@@ -694,6 +766,7 @@ export default {
     font-weight: 600;
     font-size: 28px;
     line-height: 32px;
+    text-transform: uppercase;
 
     @media(max-width: 768px) {
       font-size: 22px;
@@ -791,6 +864,7 @@ export default {
     font-weight: 600;
     font-size: 28px;
     line-height: 32px;
+    text-transform: uppercase;
 
     @media(max-width: 768px) {
       font-size: 22px;
@@ -915,6 +989,155 @@ export default {
     width: 100%;
     height: auto;
     border-radius: 30px;
+  }
+}
+.gallery {
+  width: 100%;
+  margin-top: 120px;
+
+  &__inner {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    gap: 60px;
+  }
+  &__head  {
+    width: fit-content;
+    align-self: center;
+  }
+  &__title {
+    font-weight: 600;
+    font-size: 28px;
+    line-height: 32px;
+    text-transform: uppercase;
+
+    @media(max-width: 768px) {
+      font-size: 22px;
+      line-height: 38px;
+    }
+  }
+  &__content {
+    width: 100%;
+    height: 744px;
+    display: grid;
+    grid-template-columns: 49% 23.75% 23.75%;
+    grid-column-gap: 1.5%;
+    grid-row-gap: 2.65%;
+    grid-template-rows: 31.7% 26% 37%;
+
+    @media(max-width: 1024px) {
+      height: 600px;
+    }
+  }
+  &__image {
+    border-radius: 50px;
+    background-image: url('../assets/static-images/gallery.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    &:nth-child(1) {
+      grid-row-start: 1;
+      grid-row-end: 3;
+    }
+    &:nth-child(2) {
+      grid-row-start: 1;
+      grid-row-end: 3;
+    }
+    &:nth-child(6) {
+      grid-row-start: 2;
+      grid-row-end: 4;
+    }
+  }
+  &__link {
+    align-self: center;
+    background: #FFFFFF;
+    border: 1px solid $rose_light;
+    border-radius: 53px;
+    padding: 16px 32px 14px;
+    color: $rose_light;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 21px;
+    text-transform: uppercase;
+    transition: all .3s ease;
+
+    &:hover {
+      color: #FFFFFF;
+      background: $rose_light;
+    }
+  }
+}
+.location {
+  width: 100%;
+  margin-top: 120px;
+
+  &__inner {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    gap: 60px;
+  }
+  &__head  {
+    width: fit-content;
+    align-self: center;
+  }
+  &__title {
+    font-weight: 600;
+    font-size: 28px;
+    line-height: 32px;
+    text-transform: uppercase;
+
+    @media(max-width: 768px) {
+      font-size: 22px;
+      line-height: 38px;
+    }
+  }
+  &__content {
+    width: 100%;
+    display: flex;
+    align-items: start;
+    justify-content: center;
+    flex-wrap: wrap;
+    column-gap: 20px;
+    row-gap: 40px;
+  }
+  &__item {
+    width: 100%;
+    max-width: 590px;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    gap: 10px;
+  }
+  &__slider {
+    width: 100%;
+    margin-bottom: 30px;
+  }
+  &__slide {
+    height: auto;
+    width: 100%;
+    border-radius: 30px;
+
+    .img {
+      width: 100%;
+      height: auto;
+      border-radius: 30px;
+    }
+  }
+  &__metro {
+    display: flex;
+    align-items: start;
+    gap: 10px;
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 28px;
+  }
+  &__address {
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 160%;
   }
 }
 
