46393e2f491ac6a9fde3956c5e6f2bc34a4d3056
t1ep1l0t <alien95111@gmail.com>
2023-04-12 08:14:12 +0300

    final update

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -61,7 +61,7 @@ const router = new VueRouter({
         if (savedPosition) {
             return savedPosition
         } else {
-            return { x: 0, y: 0 }
+            return { x: 0, y: 0, behavior: 'smooth' }
         }
     },
     routes
