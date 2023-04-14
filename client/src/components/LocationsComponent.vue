62f41e866baf79b478e9b401a7b93afa0b5ee148
t1ep1l0t <alien95111@gmail.com>
2023-04-14 20:19:11 +0300

    final update

--- client/src/components/LocationsComponent.vue
+++ client/src/components/LocationsComponent.vue
@@ -211,10 +211,14 @@ export default {
     height: auto;
     width: 100%;
     border-radius: 30px;
+    background-color: #FAFAFA;
+    display: flex!important;
+    align-items: center;
+    justify-content: center;
 
     .img {
-      width: 100%;
-      height: auto;
+      width: auto;
+      max-height: 350px;
       border-radius: 30px;
     }
   }
