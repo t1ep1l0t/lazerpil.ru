d696350349e6698b98d8df123d5e9fe14e245d7b
t1ep1l0t <alien95111@gmail.com>
2023-04-14 21:13:02 +0300

    final update

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -66,7 +66,7 @@ const store = new Vuex.Store({
 
             console.log(state.reviews_summary)
 
-            state.reviews_summary.toFixed(2)
+            Math.ceil(state.reviews_summary)
 
             console.log(state.reviews_summary)
         },
