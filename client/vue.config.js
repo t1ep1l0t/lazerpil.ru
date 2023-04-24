fef0a150947cfae586e82289d07e650ae1e9c932
t1ep1l0t <alien95111@gmail.com>
2023-04-24 14:43:39 +0300

    add google&yandex metrik

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
 })
 
-
+module.exports = {
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+          args[0].template = './public/index.html'
+          args[0].minify = false
+          return args
+        })
+  }
+};
